12ad587845366089e35f98aa8308371dbc0cce88
lukas <lukas.gehrig@students.fhnw.ch>
2020-03-16 19:08:43 +0100

    updated DA (data access) for frequency and aisle occurences

--- data/DA.py
+++ data/DA.py
@@ -5,6 +5,7 @@ Created on Thu Feb 27 19:25:15 2020
 @author: Lukas
 """
 import pandas as pd
+import os
 
 
 class DA(object):
@@ -12,32 +13,77 @@ class DA(object):
     _da = None
     
     @staticmethod
-    def get_DA():
-        #singleton pattern
+    def get_DA(nav = None):
+        """
+        Returns single instance of DA (DataAccess)
+        
+        Keyword arguments:
+        nav -- path-navigation to root (default ../)
+        """
         if not isinstance(DA._da, DA):
             DA._da = DA()
+        if isinstance(nav, str):
+            DA._da.set_nav(nav+'data/')
+        elif not isinstance(DA._da.get_nav(), str):
+            DA._da.set_nav('../data/')
         return DA._da
     
     def __init__(self):
         self._df_origin = None
-        self._df_sub = None
+        self._nav = None
+        freq, aisle, rating = 'freq', 'aisle', 'rating'
+        self._df_sub_data = {freq : None, aisle : None, rating : None}
+        self._df_sub_methods = {freq : self._red_prod_freq,
+                                aisle : self._red_prod_aisle,
+                                rating : self._red_prod_rating}
         
-    def _create_df_sub(self):
-        """Creates the specified sub-dataset"""
-        #create df out of self._df_origin (use self._get_df_origin()) in order to create the RS
-        return None
+    def _red_prod_freq(self, df = None, drop = 0.8):
+        """Keeps `keep` of the most popular products"""
+        if not isinstance(df, pd.DataFrame):
+            df = self._get_df_origin()
+        products = df.loc[:,'product_name'].value_counts()
+        ix = int(products.keys().shape[0] * (1 - drop))
+        items = products[:ix].keys()
+        df_selected = df[df['product_name'].isin(items)]
+        return df_selected.reset_index(drop = True)
+    
+    def _red_prod_aisle(self, drop = 0.8):
+        """Keeps `keep` of the most popular products per aisle"""
+        df = self._get_df_origin()
+        df_selected = pd.DataFrame(columns = df.columns)
+        df_grouped = df.groupby(['aisle_id'])
+        for aisle_id, group in df_grouped:
+            df_selected = df_selected.append(self._red_prod_freq(group, drop))
+        return df_selected
+    
+    def _red_prod_rating(self):
+        pass
         
-    def _get_df_origin(self, nav = '../'):
+    def _get_df_origin(self):
         """Lazy loader of the whole dataset"""
         if not isinstance(self._df_origin, pd.DataFrame):
-            self._df_origin = pd.read_csv(nav+'data/Recommender4Retail.csv')
+            self._df_origin = pd.read_csv(self._nav+'Recommender4Retail.csv')
+            self._df_origin = self._df_origin.drop(columns = [self._df_origin.columns[0]])
         return self._df_origin
     
-    def _get_df_sub(self):
+    def get_df_sub(self, method = 'freq'):
         """Lazy loader of the sub dataset"""
-        #if self._df_sub is None, check if file exists, if yes, load and store it, otherwise create it with the according function
-        return self._df_sub
+        if not isinstance(self._df_sub_data[method], pd.DataFrame):
+            path = self._nav+method+'.csv'
+            if os.path.exists(path):
+                self._df_sub_data[method] = pd.read_csv(path)
+            else:
+                self._df_sub_data[method] = self._df_sub_methods[method]()
+                self._df_sub_data[method].to_csv(path, index = False)
+        return self._df_sub_data[method]
     
-    def get_df_example(self):
-        """Just an example function, which creates/gets the data via data-wrangling or object loading"""
-        return None
+    def set_nav(self, nav):
+        self._nav = nav
+        
+    def get_nav(self):
+        return self._nav
+    
+    
+    
+    
+    
