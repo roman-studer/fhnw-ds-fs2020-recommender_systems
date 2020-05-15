c012a114d7562281b0cde7f9bdc1ae51a444dc9d
Ryzash <56029241+Ryzash@users.noreply.github.com>
2020-05-15 17:52:13 +0200

    changed gitignore

--- data/reduce_csv.py
+++ data/reduce_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-chunk = pd.read_csv('Recommender4Retail_big.csv', nrows=1_000_000)
+chunk = pd.read_csv('Recommender4Retail_big.csv', nrows=10_00_000)
 chunk = chunk.loc[:,['user_id', 'order_id', 'product_name', 'product_id', 'reordered', 'aisle_id']]
 print(chunk.head())
 if not Path('Recommender4Retail.csv').is_file():
