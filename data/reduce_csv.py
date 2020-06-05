fabbdca7241511dbc7dd405790c4dfd9d6635ce4
lukas <lukas.gehrig@students.fhnw.ch>
2020-06-05 11:30:13 +0200

    deleted Interaction_Matrix.ipynb, deleted Production_Reduction_via_Rating.ipynb, deleted reduce_csv.py, modified SVD text and rendered output in Documentation.ipynb

--- data/reduce_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-chunk = pd.read_csv('Recommender4Retail_big.csv', nrows=10_00_000)
-chunk = chunk.loc[:,['user_id', 'order_id', 'product_name', 'product_id', 'reordered', 'aisle_id']]
-print(chunk.head())
-if not Path('Recommender4Retail.csv').is_file():
-    chunk.to_csv('Recommender4Retail.csv', mode='a', header=True, index=True)
